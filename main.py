05f3d872525be2f790e4a45878496d2af23238f9
Elyasin Sadri <elyasinson1@yahoo.com>
2023-02-22 19:39:05 +0000

    Added some buttons in row and columns

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 #Imports
-from tkinter import *
+import tkinter as tk
 from tkinter import messagebox
 
 #Window
-window = Tk()
+window = tk.Tk()
 window.title("Tkinter Project")
 window.geometry("300x300")
 
 #Functions
 def HelloEO():
   messagebox.showinfo("BF", "Hello whoever u R!")
-
+  
 #Adjustments
-HGit = Label(text="Hello Github Community!")
-HGit.pack()
-HRpl = Label(text="Hello Replit Commuinty!")
-HRpl.pack()
-button = Button(text="Click me!", command = HelloEO)
-button.pack()
+for i in range(3):
+  window.columnconfigure(i, weight = 1, minsize = 75)
+  window.rowconfigure(i, weight = 1, minsize = 50)
+  
+  for j in range(0, 3):
+    frame = tk.Frame(
+      master = window,
+      relief = tk.RAISED,
+      borderwidth = 1
+    )
+    frame.grid(row = i, column = j, padx = 5, pady = 5)
+    INFO = tk.Button(master = frame, text = f"Row {i}\nColumn {j}", command = HelloEO)
+    INFO.pack()
 
 #Mainloop
-mainloop()
+tk.mainloop()
